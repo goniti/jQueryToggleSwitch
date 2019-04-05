03ca6f4a0aa439eb020e51103306a0339e5fa56d
Carlos <goniti@live.fr>
2019-04-05 19:28:00 +0200

    Toggle Switch jQuery patch 1.1

--- toggleSwitch.js
+++ toggleSwitch.js
@@ -1,31 +1,40 @@
-// jQuery Toggle switch
+// jQuery Toggle Switch
+
 $(document).ready(function () {
+
+  const a = "active";
   const t = $("#true");
   const f = $("#false");
   const s = $("#switcher");
-  const selected = "active";
 
-  // on Switcher
-  s.click(function () {
-    f.toggleClass(selected);
-    t.toggleClass(selected);
-  });
-  // on False
-  f.click(function () {
-    s.prop("checked", true);
-    f.addClass(selected);
-    t.removeClass(selected);
-  });
-  //on True
+  // click on True
   t.click(function () {
     s.prop("checked", false);
-    t.addClass(selected);
-    f.removeClass(selected);
+    t.addClass(a);
+    f.removeClass(a);
+    foo() //execute foo function if click on True
   });
 
-  const value = [1, 0];
+  // click on False
+  f.click(function () {
+    s.prop("checked", true);
+    f.addClass(a);
+    t.removeClass(a);
+    foo() //execute foo function if click on False
+  });
 
+  // click on Switch
+  s.click(function () {
+    f.toggleClass(a);
+    t.toggleClass(a);
+    foo() //execute foo function if click on switch
+  });
+
+
+  // Get value
   function foo() {
+    const value = [1, 0];
+
     if (s.prop("checked")) {
       result = value[1]
     } else {
@@ -34,6 +43,7 @@ $(document).ready(function () {
     const myResult = result;
     $('.output').html(myResult);
   }
-  foo()
+
+  foo() //execute foo function on load
 
 });
